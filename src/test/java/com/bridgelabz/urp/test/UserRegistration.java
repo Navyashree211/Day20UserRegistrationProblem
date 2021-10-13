72943e4ce8e7ad9178577e545e676de58fa43d2c
navyas211@gmail.com <navyas211@gmail.com>
2021-10-13 20:10:18 +0530

    [NAVYA]ADDED: Enter First name starting with capital and has minimum 3 characters in it

---
+++ src/test/java/com/bridgelabz/urp/test/UserRegistration.java
@@ -0,0 +1,21 @@
+/**
+ * Valid First Name with first letter starting with capital and has minimum 3 characters in it.
+ *
+ * @author: Navya Shree
+ * @since: 13.10.2021
+ */
+package com.bridgelabz.urp.test;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println(firstName("Navya"));
+		System.out.println(firstName("Anokhi"));
+	}
+
+	// validate first name
+	public static boolean firstName(String FirstName) {
+		return FirstName.matches("^[A-Z]{1}[a-z]{2,}$");
+
+	}
+
+}
